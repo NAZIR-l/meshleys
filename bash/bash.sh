dab2e688600f8434bc122221417255b13b2d66cf
NAZIR-l <NAZIR.ALKAHWAJI@gmail.com>
2022-12-29 16:14:04 +0300

    testing3

--- bash/bash.sh
+++ bash/bash.sh
@@ -6,8 +6,8 @@ const { Amplify, Auth } = require('aws-amplify') ;
 
 
 program
-.command('build', 'docker  build .',{ executableFile: 'bash2.sh' })
-.command('login', 'login to aws', { executableFile: 'bash3.sh' })
-.command('create', 'test to aws', { executableFile: 'test.sh' })
+.command('build', 'docker  build .',{ executableFile: 'docker.sh' })
+.command('login', 'login to aws', { executableFile: 'login.sh' })
+.command('create', 'test to aws', { executableFile: 'create.sh' })
 
 program.parse(process.argv);
