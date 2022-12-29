3c1a7e035f58e3d30259e7a40eae766659a7d024
NAZIR-l <NAZIR.ALKAHWAJI@gmail.com>
2022-12-29 16:19:08 +0300

    g

--- bash/bash.sh
+++ bash/bash.sh
@@ -8,6 +8,6 @@ const { Amplify, Auth } = require('aws-amplify') ;
 program
 .command('build', 'docker  build .',{ executableFile: 'docker.sh' })
 .command('login', 'login to aws', { executableFile: 'login.sh' })
-.command('create', 'create repository github', { executableFile: 'create.sh' })
+.command('create', 'create repository in github', { executableFile: 'create.sh' })
 
 program.parse(process.argv);
